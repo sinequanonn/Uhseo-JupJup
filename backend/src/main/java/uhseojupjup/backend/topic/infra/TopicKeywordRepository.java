7eee71044b8245e08762d7a574bb26a0903a602a
sinequanonn <dleck288@gmail.com>
2026-06-27 11:33:12 +0900

    feat(topic): topic_keyword 페치조인 쿼리 추가

--- backend/src/main/java/uhseojupjup/backend/topic/infra/TopicKeywordRepository.java
+++ backend/src/main/java/uhseojupjup/backend/topic/infra/TopicKeywordRepository.java
@@ -1,6 +1,7 @@
 package uhseojupjup.backend.topic.infra;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uhseojupjup.backend.topic.domain.TopicKeyword;
 
 import java.util.List;
@@ -10,4 +11,10 @@ public interface TopicKeywordRepository extends JpaRepository<TopicKeyword, Long
     List<TopicKeyword> findByTopicId(Long topicId);
 
     List<TopicKeyword> findByKeywordId(Long keywordId);
+
+    @Query("select tk from TopicKeyword tk join fetch tk.keyword where tk.topic.id = :topicId order by tk.keyword.name")
+    List<TopicKeyword> findWithKeywordByTopicId(Long topicId);
+
+    @Query("select tk from TopicKeyword tk join fetch tk.topic where tk.keyword.id = :keywordId order by tk.topic.id")
+    List<TopicKeyword> findWithTopicByKeywordId(Long keywordId);
 }
