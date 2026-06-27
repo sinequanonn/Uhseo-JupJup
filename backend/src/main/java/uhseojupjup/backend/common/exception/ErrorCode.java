e2bacb45c3a7037e396657dacbde262d0a5156c0
sinequanonn <dleck288@gmail.com>
2026-06-27 11:33:12 +0900

    feat(common): TOPIC_NOT_FOUND, KEYWORD_NOT_FOUND 에러코드 추가

--- backend/src/main/java/uhseojupjup/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/uhseojupjup/backend/common/exception/ErrorCode.java
@@ -18,7 +18,10 @@ public enum ErrorCode {
     INVALID_ID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증 토큰입니다."),
     EXPIRED_ID_TOKEN(HttpStatus.UNAUTHORIZED, "인증 토큰이 만료되었습니다."),
     EMAIL_NOT_VERIFIED(HttpStatus.FORBIDDEN, "검증되지 않은 이메일입니다."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
+
+    TOPIC_NOT_FOUND(HttpStatus.NOT_FOUND, "토픽을 찾을 수 없습니다."),
+    KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "키워드를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
