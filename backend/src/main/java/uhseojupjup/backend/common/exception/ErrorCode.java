d41ecb646f51f9fb1cde98f6c24fb78097426581
sinequanonn <dleck288@gmail.com>
2026-06-27 15:00:15 +0900

    feat(common): BLOG_NOT_FOUND 에러코드 추가

--- backend/src/main/java/uhseojupjup/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/uhseojupjup/backend/common/exception/ErrorCode.java
@@ -21,7 +21,8 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
 
     TOPIC_NOT_FOUND(HttpStatus.NOT_FOUND, "토픽을 찾을 수 없습니다."),
-    KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "키워드를 찾을 수 없습니다.");
+    KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "키워드를 찾을 수 없습니다."),
+    BLOG_NOT_FOUND(HttpStatus.NOT_FOUND, "블로그를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
